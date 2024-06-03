3878f8e6150d999942db78e0943f1ddde5eaea1d
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-03 18:28:13 +0530

    Exit & Complete Button GUI

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,17 +1,26 @@
 import FreeSimpleGUI as hawwww
+import zipcreator
 
 label = hawwww.Text("Select files to compress:")
 input1 = hawwww.Input()
-choose_button1 = hawwww.FilesBrowse("Choose Files")
+choose_button1 = hawwww.FilesBrowse("Choose Files", key="files")
 
 label2 = hawwww.Text("Select destination folder:")
 input2 = hawwww.Input()
-choose_button2 = hawwww.FolderBrowse("Choose Destination")
+choose_button2 = hawwww.FolderBrowse("Choose Destination", key="folder")
 
 compress_button = hawwww.Button("Compress")
+output_label = hawwww.Text(key="output")
 
-window = hawwww.Window("File Compressor", layout= ([label, input1, choose_button1],
-                                                   [label2, input2, choose_button2],
-                                                   [compress_button]))
-window.read()
+window = hawwww.Window("File Compressor", layout=([label, input1, choose_button1],
+                                                  [label2, input2, choose_button2],
+                                                  [compress_button, output_label]))
+while True:
+    event, values = window.read()
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    zipcreator.make_archive(filepaths, folder)
+    window["output"].update("The file has been compressed!", text_color="Blue")
+
+
 window.close()
--- gui.py
+++ gui.py
@@ -10,7 +10,11 @@ list_box = slim_shady.Listbox(values=functions.get_todos("userdatafiles/todos.tx
                               enable_events=True,
                               size=[45, 5])
 edit_button = slim_shady.Button("Edit")
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+complete_button = slim_shady.Button("Complete")
+exit_button = slim_shady.Button("Exit")
+
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
+
 window = slim_shady.Window("To-DoWave", layout=layout,
                            font=("Helvetica", 18))
 while True:
@@ -22,17 +26,27 @@ while True:
             todos.append(new_todo)
             functions.writelines_todos("userdatafiles/todos.txt", todos)
             window['to-dos'].update(values=todos)
+            window['to-do'].update(value="")
 
         case "Edit":
             todos = functions.get_todos("userdatafiles/todos.txt")
-
             todo_to_edit = values["to-dos"][0]
             new_todo = values["to-do"]
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
-
             functions.writelines_todos("userdatafiles/todos.txt", todos)
             window['to-dos'].update(values=todos)
+
+        case "Complete":
+            todos = functions.get_todos("userdatafiles/todos.txt")
+
+            todo_to_complete = values["to-dos"][0]
+            todos.remove(todo_to_complete)
+            functions.writelines_todos("userdatafiles/todos.txt", todos)
+            window['to-dos'].update(values=todos)
+            window['to-do'].update(value="")
+        case "Exit":
+            exit()
         case "to-dos":
             window["to-do"].update(value=values['to-dos'][0])
         case slim_shady.WIN_CLOSED:
