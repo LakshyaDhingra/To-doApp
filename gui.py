d9d051671fd4111064d8901e251acb4cc7a5d0b0
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-04 15:12:37 +0530

    Theme and Exit Button GUI

--- gui.py
+++ gui.py
@@ -2,23 +2,29 @@ import functions
 
 import FreeSimpleGUI as slim_shady
 
+import time
+
+slim_shady.theme("Black")
+
+clock = slim_shady.Text(key="clock")
 label = slim_shady.Text("Type to add, show, edit, tick off or exit: ")
 input_box = slim_shady.InputText(tooltip="Enter a to-do: ", key="to-do")
-add_button = slim_shady.Button("Add")
+add_button = slim_shady.Button("Add", size=10)
 list_box = slim_shady.Listbox(values=functions.get_todos("userdatafiles/todos.txt"),
                               key="to-dos",
                               enable_events=True,
                               size=[45, 5])
-edit_button = slim_shady.Button("Edit")
-complete_button = slim_shady.Button("Complete")
-exit_button = slim_shady.Button("Exit")
+edit_button = slim_shady.Button("Edit", size=10)
+complete_button = slim_shady.Button("Complete", size=10)
+exit_button = slim_shady.Button("Exit", size=10)
 
-layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
+layout = [[clock], [label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
 
 window = slim_shady.Window("To-DoWave", layout=layout,
                            font=("Helvetica", 18))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=20)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos("userdatafiles/todos.txt")
@@ -29,13 +35,16 @@ while True:
             window['to-do'].update(value="")
 
         case "Edit":
-            todos = functions.get_todos("userdatafiles/todos.txt")
-            todo_to_edit = values["to-dos"][0]
-            new_todo = values["to-do"]
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.writelines_todos("userdatafiles/todos.txt", todos)
-            window['to-dos'].update(values=todos)
+            try:
+                todos = functions.get_todos("userdatafiles/todos.txt")
+                todo_to_edit = values["to-dos"][0]
+                new_todo = values["to-do"]
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.writelines_todos("userdatafiles/todos.txt", todos)
+                window['to-dos'].update(values=todos)
+            except IndexError:
+                slim_shady.popup("Please select an item first!", font=("Helvetica", 17), title="Error!")
 
         case "Complete":
             todos = functions.get_todos("userdatafiles/todos.txt")
@@ -46,10 +55,9 @@ while True:
             window['to-dos'].update(values=todos)
             window['to-do'].update(value="")
         case "Exit":
-            exit()
+            break
         case "to-dos":
             window["to-do"].update(value=values['to-dos'][0])
         case slim_shady.WIN_CLOSED:
-            exit()
-
+            break
 window.close()
