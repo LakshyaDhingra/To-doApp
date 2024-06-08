fa3e863ecb995b57ccc7d1ab79cd6addc8936e44
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-08 20:09:12 +0530

    Zip Compressor & Extractor

--- bonus/bonus18.py
+++ bonus/bonus18.py
@@ -2,5 +2,18 @@ import streamlit as webcam
 
 from PIL import Image
 
-camera_image = webcam.camera_input("Camera")
+with webcam.expander("Start Camera"):
+    # Starting the camera
+    camera_image = webcam.camera_input("Camera")
+
+
+if camera_image:
+    # Create image instance from PIL
+    img = Image.open(camera_image)
+
+    # Conversion to grayscale
+    gray_img = img.convert("L")
+
+    # Displaying grayscale image
+    webcam.image(gray_img)
 
